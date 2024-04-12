05751533d345769076ef97c81a7fa443db0fb626
xMellylicious <marshm3lly@gmail.com>
2024-04-13 00:11:22 +0100

    Movement engine fixes

--- ReplicatedStorage/Controllers/MovementEngine.Controller.lua
+++ ReplicatedStorage/Controllers/MovementEngine.Controller.lua
@@ -23,22 +23,9 @@ local player = PS.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local hum = character:WaitForChild("Humanoid")
 
--- Camera
-local camera = workspace.CurrentCamera
-local cameraX = workspace.CurrentCamera.CFrame.X
-local cameraY = workspace.CurrentCamera.CFrame.Y
-
--- Configurable Variables
-local maximumLean = 1.25
-local minimumLean = -1.25
-local changeLeanBy = 1.25
-
--- System Variables
-local currentLean = 0
-
 local movement_engine = {}
 
-function Handle(stance, inputState:Enum.UserInputState, inputObject: InputObject)
+function movement_engine.Handle(stance, inputState:Enum.UserInputState, inputObject: InputObject)
     if stance == "sprint" and inputState == Enum.UserInputState.Begin then
         remotes.StartSprinting:FireServer()
     elseif stance == "sprint" and inputState == Enum.UserInputState.End then
@@ -46,9 +33,8 @@ function Handle(stance, inputState:Enum.UserInputState, inputObject: InputObject
     end
 end
 
-function movement_engine:Setup()
-    CAS:BindAction("sprint", Handle, true, Enum.KeyCode.LeftShift)
+function movement_engine:Initialise()
+    CAS:BindAction("sprint", self.Handle, true, Enum.KeyCode.LeftShift)
 end
 
-wait(1)
-movement_engine:Setup()
+return movement_engine
